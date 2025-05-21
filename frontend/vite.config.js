9ce99878285c3f74b5396b6b013fcb6e74d4f548
Varilla <44826959+adityaamalik@users.noreply.github.com>
2025-05-21 22:01:18 +0200

    vite config updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,17 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5001",
-        changeOrigin: true,
-      },
-    },
-  },
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    emptyOutDir: true,
-  },
 });
